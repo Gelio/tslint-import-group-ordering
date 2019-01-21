e668a4b5390a08979581c96dfb8b678a15c98b68
Grzegorz Rozdzialik <grzegorz.rozdzialik@codilime.com>
2019-01-21 10:10:10 +0100

    fix automated autofix test

--- test/automated-fix/test-automated-fix.sh
+++ test/automated-fix/test-automated-fix.sh
@@ -1,5 +1,10 @@
 cp cases cases-backup -r
-npx tslint --fix cases
+
+(cd ../.. && npx tslint test/automated-fix/cases/tslint.json test/automated-fix/cases/*.ts --fix)
+
+echo "Diff between the test cases and the expected version. If empty, then the test passed"
+echo
+
 diff -bur --color cases expected
 rm cases -r
 mv cases-backup cases
