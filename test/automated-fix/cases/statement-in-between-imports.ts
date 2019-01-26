57315ed69a9b8e0dcfd9f8b236aacfa9c354f028
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-26 19:41:24 +0100

    add test for preserving statements in between imports

---
+++ test/automated-fix/cases/statement-in-between-imports.ts
@@ -0,0 +1,6 @@
+import {} from 'fs';
+
+const variable = 5;
+
+import {} from './relative';
+import {} from 'products';
---
+++ test/automated-fix/expected/statement-in-between-imports.ts
@@ -0,0 +1,7 @@
+import {} from 'fs';
+
+import {} from 'products';
+
+import {} from './relative';
+
+const variable = 5;
