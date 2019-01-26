413612402e99081858d891d560934ffee6ec06fa
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-26 19:41:13 +0100

    add test for preserving comments

---
+++ test/automated-fix/cases/preserve-comments.ts
@@ -0,0 +1,7 @@
+import {} from 'fs';
+/**
+ * Comment above common
+ */
+import {} from 'common';
+// Inline comment above products
+import {} from 'products/some/path/here';
---
+++ test/automated-fix/expected/preserve-comments.ts
@@ -0,0 +1,9 @@
+import {} from 'fs';
+
+/**
+ * Comment above common
+ */
+import {} from 'common';
+
+// Inline comment above products
+import {} from 'products/some/path/here';
