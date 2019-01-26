07aeb043febae58b1c23bc135fd6cc4cf38c97fe
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-26 19:32:25 +0100

    add an autofix test for the matching order

---
+++ test/automated-fix/cases/matching-order.ts
@@ -0,0 +1,7 @@
+import {} from 'util';
+
+import {} from 'fabric';
+
+import {} from 'product';
+
+import {} from './relative';
---
+++ test/automated-fix/expected/matching-order.ts
@@ -0,0 +1,7 @@
+import {} from 'fabric';
+
+import {} from 'util';
+
+import {} from 'product';
+
+import {} from './relative';
