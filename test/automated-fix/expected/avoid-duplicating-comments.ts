e7aa90014290177b90316afdcee72a3e28192c74
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-27 10:54:43 +0100

    add an auto-fix test for avoiding duplicating the imports

---
+++ test/automated-fix/expected/avoid-duplicating-comments.ts
@@ -0,0 +1,7 @@
+import {} from 'common';
+
+/**
+ * This import is attached to the "./relative" import and should not be duplicated
+ * when the autofix is applied.
+ */
+import {} from './relative';
